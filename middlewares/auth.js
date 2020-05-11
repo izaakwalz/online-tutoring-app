c46702c5b8046ee00ee620a4973f2a2171ee896d
izaakwalz <iblack.xyz@gmail.com>
2020-05-11 18:46:51 +0100

    updated auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const Tutor = require('../models/Tutor-Model');
 
 exports.isAuthAdmin = (req, res, next) => {
   try {
@@ -7,12 +8,12 @@ exports.isAuthAdmin = (req, res, next) => {
     if (!token)
       return res
         .status(401)
-        .json({ error: 'No access tooken, permission not granted!' });
+        .json({ error: 'No access token, permission not granted!' });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (decoded.tutor.isAdmin == true) {
-      req.team = decoded;
+    if (decoded.data.isAdmin == true) {
+      req.team = decoded.data;
       next();
     } else {
       res
@@ -21,7 +22,7 @@ exports.isAuthAdmin = (req, res, next) => {
     }
   } catch (err) {
     console.log(err);
-    res.status(400).json({ msg: 'Token not valid' });
+    res.status(400).json({ message: 'Token not valid' });
   }
 };
 
@@ -32,16 +33,12 @@ exports.isAuthTutor = (req, res, next) => {
     if (!token)
       return res
         .status(401)
-        .json({ error: 'No access tooken, permission not granted!' });
+        .json({ error: 'No access token, permission not granted!' });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    if (
-      decoded.tutor.isAdmin == false ||
-      decoded.tutor.isAdmin == true ||
-      decoded.tutor.isActive == true
-    ) {
-      req.tutor = decoded.tutor;
+    if (decoded.data.role == 'tutor' && decoded.data.isActive == true) {
+      req.tutor = decoded.data;
       next();
     } else {
       res
@@ -50,7 +47,7 @@ exports.isAuthTutor = (req, res, next) => {
     }
   } catch (err) {
     console.log(err);
-    res.status(400).json({ msg: 'Token not valid' });
+    res.status(400).json({ message: 'Token not valid' });
   }
 };
 
@@ -61,16 +58,47 @@ exports.isAuthStundent = (req, res, next) => {
     if (!token)
       return res
         .status(401)
-        .json({ error: 'No access tooken, permission not granted!' });
+        .json({ error: 'No access token, permission not granted!' });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = decoded;
+    if (decoded.data.role == 'stundent') {
+      req.stundent = decoded.data;
+      next();
+    } else {
+      res
+        .status(400)
+        .json({ error: 'You are not authorized to view this route' });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: 'Token not valid' });
+  }
+};
+
+exports.isAuthDeactivated = async (req, res, next) => {
+  try {
+    const token = req.header('x-auth-token');
+
+    if (!token)
+      return res
+        .status(401)
+        .json({ error: 'No access token, permission not granted!' });
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = decoded.data.isActive;
+    console.log(user);
+    if (user == false) {
+      res.status(401).json({
+        error:
+          'You have been deactivated 🤨🤨 by the admin, please contact the admin',
+      });
+    }
     next();
   } catch (err) {
     console.log(err);
-    res
-      .status(400)
-      .json({ error: 'You are not authorized to view this route' });
+    res.status(400).json({ Error: 'Server Error' });
+    process.exit(1);
   }
 };
