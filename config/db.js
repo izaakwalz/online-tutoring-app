32c627c5de36011e4a474780f968f40e0314694b
izaakwalz <iblack.xyz@gmail.com>
2020-05-13 00:21:32 +0100

    ready to deploy again

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      `${process.env.MONGODB_URI_Atlas_Connection}`,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false, //! Mongoose: deprecated. stuff, for future use updateOne()
+      }
+    );
+    console.log(`MongoDB Connected: localhost`);
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
